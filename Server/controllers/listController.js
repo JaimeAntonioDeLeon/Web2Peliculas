a9937442ecee7ba8c14fb05cfcefd1d205ac8ef5
jaime.deleonch <jaime.deleonch@uanl.edu.mx>
2024-10-22 17:26:03 -0600

    wip-many things have been added

--- Server/controllers/listController.js
+++ Server/controllers/listController.js
@@ -1,101 +1,147 @@
-const List = require('../models/list.js');
+const List = require("../models/list.js");
 
-const getLists = async (req, res)=>{
-    try{
-        const lists = await List.find({});
-        res.json(lists);
-    }
-    catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
+const getLists = async (req, res) => {
+  try {
+    const lists = await List.find({});
+    res.json(lists);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-const getListSpecific = async (req, res)=>{
-    try{
-        const {id} = req.body;
-        const lists = await List.findOne({"_id":id}).populate('movies').populate('user_id');
-        res.status(200).json(lists);
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
+const getListSpecific = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const lists = await List.findOne({ _id: id })
+      .populate("movies")
+      .populate("user_id");
+    res.status(200).json(lists);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-const getListsMovie = async (req, res)=>{
-    try{
-        const {movie_id} = req.body;
-        const lists = await List.find({movies:movie_id});
-        res.json(lists);
-    }
-    catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
+const getListsMovie = async (req, res) => {
+  try {
+    const { movie_id } = req.body;
+    const lists = await List.find({ movies: movie_id });
+    res.json(lists);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-const getListsUser = async (req, res)=>{
-    try{
-        const {id} = req.body;
-        const lists = await List.find({user_id:id});
-        res.json(lists);
-    }
-    catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
+const getListsUser = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const lists = await List.find({ user_id: id });
+    res.json(lists);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-const postCreateList = async(req, res) => {
-    try{
-        const lists = await List.create(req.body);
-        res.status(200).json(lists);
-    }catch(error){
-        res.status(500).json({message: error.message});
-    } 
+const getListsUserMovieFilter = async (req, res) => {
+  try {
+    const { id, movie } = req.body;
+    const lists = await List.find({ user_id: id, movies: { $ne: movie } });
+    res.json(lists);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const postCreateList = async (req, res) => {
+  try {
+    const lists = await List.create(req.body);
+    res.status(200).json(lists);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const listAddMovie = async (req, res) => {
+  try {
+    const { id, movie } = req.body;
+    const post = await List.updateOne(
+      { _id: id },
+      {
+        $push: {
+          movies: movie,
+        },
+      }
+    );
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const listRemoveMovie = async (req, res) => {
+  try {
+    const { id, movie } = req.body;
+    const post = await List.updateOne(
+      { _id: id },
+      {
+        $pullAll: {
+          movies: [{ _id: movie }],
+        },
+      }
+    );
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const listUpdate = async (req, res) => {
-    try {
-      const { id, title } = req.body;
-      const post = await List.updateOne(
-        { _id: id },
-        {
-          $set: {
-            title: title
-          },
-        }
-      );
-      res.status(200).json(post);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
-
-  const deleteList = async (req, res)=>{
-    try{
-        //const {id} = req.params;
-        const post = await List.deleteOne(req.body);
-        res.status(200).json(post);
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
+  try {
+    const { id, title } = req.body;
+    const post = await List.updateOne(
+      { _id: id },
+      {
+        $set: {
+          title: title,
+        },
+      }
+    );
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-const getListSearch = async (req, res)=>{
-    try{
-        const {title} = req.body;
-        const tit = new String(title);
-        const comment = await List.find({ 'title' : { '$regex' : tit, '$options' : 'i' } });
-        res.status(200).json(comment);
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-}
+const deleteList = async (req, res) => {
+  try {
+    //const {id} = req.params;
+    const post = await List.deleteOne(req.body);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-module.exports ={
-    getLists,
-    getListSpecific,
-    postCreateList,
-    listUpdate,
-    deleteList,
-    getListsMovie,
-    getListsUser,
-    getListSearch
-}
+const getListSearch = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const tit = new String(title);
+    const comment = await List.find({ title: { $regex: tit, $options: "i" } });
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getLists,
+  getListSpecific,
+  postCreateList,
+  listUpdate,
+  deleteList,
+  getListsMovie,
+  getListsUser,
+  getListSearch,
+  listAddMovie,
+  listRemoveMovie,
+  getListsUserMovieFilter,
+};
